e406d195604449056a8424e8fd50e873f0abe61a
echan262 <131998018+echan262@users.noreply.github.com>
2025-12-06 20:59:26 +0700

    Update artikel dan perbaikan style

--- public/articles/arduino-ultrasonic.js
+++ public/articles/arduino-ultrasonic.js
@@ -8,7 +8,7 @@ const ARTICLE = {
         "img/ultrasonic2.jpg",
         "img/ultrasonic3.jpg"
     ],
-    content: "<p>Belajar membuat sensor jarak otomatis dengan HC-SR04.</p>",
+    content: "<p> yes 123 Belajar membuat sensor jarak otomatis dengan HC-SR04.</p>",
     sub: [
         { subtitle: "Pengantar", body: "<p>Sensor ultrasonic HC-SR04 digunakan untuk mengukur jarak.</p>" },
         { subtitle: "Contoh Project", body: "<p>Membuat robot obstacle avoidance menggunakan HC-SR04 dan Arduino.</p>" }
