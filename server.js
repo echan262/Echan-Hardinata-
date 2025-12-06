13e17de88a48c24dded23f8a59d540c278669fc1
echan262 <131998018+echan262@users.noreply.github.com>
2025-12-06 14:03:27 +0700

    Fix project structure

--- server.js
+++ server.js
@@ -22,3 +22,27 @@ io.on("connection", (socket) => {
 http.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.on("join", (data) => {
+        io.emit("chat message", {
+            username: "System",
+            avatar: "",
+            text: `${data.username} joined the chat`
+        });
+    });
+
+    socket.on("chat message", (data) => {
+        socket.broadcast.emit("chat message", data);
+    });
+
+    socket.on("typing", (username) => {
+        socket.broadcast.emit("typing", username);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
