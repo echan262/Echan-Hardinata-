d08e297a92cce4f6c388f85e18fd01900a8d9982
echan262 <131998018+echan262@users.noreply.github.com>
2025-12-06 17:06:20 +0700

    Fix project structure

--- public/articles/arduino-ultrasonic.js
+++ public/articles/arduino-ultrasonic.js
@@ -3,6 +3,11 @@ const ARTICLE = {
     title: "Arduino Ultrasonic: Cara Kerja & Contoh Project",
     tags: ["Arduino","Sensor","HC-SR04"],
     thumbnail: "img/thumbnail1.jpg",
+    gallery: [
+        "img/ultrasonic1.jpg",
+        "img/ultrasonic2.jpg",
+        "img/ultrasonic3.jpg"
+    ],
     content: "<p>Belajar membuat sensor jarak otomatis dengan HC-SR04.</p>",
     sub: [
         { subtitle: "Pengantar", body: "<p>Sensor ultrasonic HC-SR04 digunakan untuk mengukur jarak.</p>" },
